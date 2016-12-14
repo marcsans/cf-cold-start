219f739843facc591060873ea818dd96aa96064f
MarcSzafraniec <mszafraniec@laposte.net>
2016-12-14 16:30:50 +0100

    Creation of Create_R

---
+++ src/Create_R.py
@@ -0,0 +1,24 @@
+
+# coding: utf-8
+
+# In[3]:
+
+import numpy as np
+import pandas as pd
+
+
+# In[6]:
+
+movies = pd.read_csv("../data/ml-latest-small/movies.csv")
+ratings = pd.read_csv("../data/ml-latest-small/ratings.csv")
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
