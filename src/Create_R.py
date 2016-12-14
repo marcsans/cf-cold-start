c46bc31dc5207cf5114801cf73ebf28236fae4da
MarcSzafraniec <mszafraniec@laposte.net>
2016-12-14 19:09:55 +0100

    Finished Create_R

--- src/Create_R.py
+++ src/Create_R.py
@@ -1,50 +1,23 @@
 
 # coding: utf-8
 
-# In[3]:
+# In[24]:
 
-import numpy as np
-import pandas as pd
-
-
-# In[6]:
-
-movies = pd.read_csv("../data/ml-latest-small/movies.csv")
-ratings = pd.read_csv("../data/ml-latest-small/ratings.csv")
-
-
-# In[8]:
-
-movies.head()
-
-
-# In[36]:
-
-ratings.head()
-
-
-# In[37]:
-
-np.asarray(ratings.head())[:,0]
-
-
-# In[35]:
-
-R = np.zeros([len(np.unique(ratings['userId'])),len(movies)])
-
-
-# In[38]:
-
-ratingsnp = np.asarray(ratings)
-
-
-# In[41]:
-
-for i in range(len(ratings)):
-    R[ratingsnp[i,0], ratingsnp[i,1]] = ratingsnp[i,2]
-
-
-# In[ ]:
-
-
+def create_R(moviesstr = "../data/ml-latest-small/movies.csv", ratingsstr = "../data/ml-latest-small/ratings.csv"):
+    
+    import numpy as np
+    import pandas as pd
+    
+    movies = pd.read_csv(moviesstr)
+    ratings = pd.read_csv(ratingsstr)
+    
+    ratings['TrueMovieId'] = ratings['movieId'].map(lambda i: movies[movies.movieId == i].index.tolist()[0])
+    R = np.zeros([len(np.unique(ratings['userId'])),len(movies)])
+    
+    ratingsnp = np.asarray(ratings)
+    
+    for i in range(len(ratings)):
+        R[ratingsnp[i,0]-1, ratingsnp[i,-1]] = ratingsnp[i,2]
+    
+    return R
 
