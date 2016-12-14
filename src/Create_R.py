4aa9093549569ac41a4d7e79329f001c4884fa36
MarcSzafraniec <mszafraniec@laposte.net>
2016-12-14 16:47:25 +0100

    Begin to extract csv

--- src/Create_R.py
+++ src/Create_R.py
@@ -13,9 +13,35 @@ movies = pd.read_csv("../data/ml-latest-small/movies.csv")
 ratings = pd.read_csv("../data/ml-latest-small/ratings.csv")
 
 
-# In[ ]:
+# In[8]:
+
+movies.head()
 
 
+# In[36]:
+
+ratings.head()
+
+
+# In[37]:
+
+np.asarray(ratings.head())[:,0]
+
+
+# In[35]:
+
+R = np.zeros([len(np.unique(ratings['userId'])),len(movies)])
+
+
+# In[38]:
+
+ratingsnp = np.asarray(ratings)
+
+
+# In[41]:
+
+for i in range(len(ratings)):
+    R[ratingsnp[i,0], ratingsnp[i,1]] = ratingsnp[i,2]
 
 
 # In[ ]:
