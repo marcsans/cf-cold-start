ed82be40706c6c329b871f3161f470fb064fc3d3
Marc SANSELME <marc.sanselme@gmail.com>
2017-01-02 15:57:26 +0100

    correction similarity 2

--- src/similarity.py
+++ src/similarity.py
@@ -12,9 +12,10 @@ import pandas as pd
 def similarity(U):
     num_users = U.shape[0]
     sim = np.zeros((num_users, num_users))
+    min_dist = 0.001
     for i in range(num_users):
         for j in range(i):
-            s = np.arccos(np.dot(U[i,:], U[j,:].T))/(np.linalg.norm(U[i,:])*np.linalg.norm(U[j,:])) / np.pi
+            s = 1 / (min_dist + np.arccos(np.dot(U[i,:], U[j,:].T))/(np.linalg.norm(U[i,:])*np.linalg.norm(U[j,:])) / np.pi)
             sim[i,j] = s
             sim[j,i] = s
     return sim
